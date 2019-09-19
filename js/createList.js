e8b708a7ec3e7214f1ef6ba98fb6a0f031d756e7
Arthur Nakano <arthurnakano@Leticias-MacBook-Air.local>
2019-09-19 17:03:14 -0300

    definicao de variaveis no javascript

--- js/createList.js
+++ js/createList.js
@@ -1,3 +1,35 @@
 (function () { 
-    console.log("hello")
+
+    const lists = [
+        {
+            tarefa: "1",
+            tasks: [
+                {
+                    tarefa: "1"
+                }
+            ]
+        }
+    ]
+
+    function createListsHtml(lists) {
+        const firstList = document.getElementById("firstList")
+        const firstTask = document.getElementById("firstTask")
+    }
+
+
+
+
+    const addListButton = document.getElementById("addList")
+    const list2 = {
+        tarefa: "5",
+    }
+
+    lists[0].tasks.push(list2)
+    console.log(lists)
+    function addListButtonClick() {
+        const nameListInput = document.getElementsByClassName("nameList")
+    }
+
+
+
 })();
