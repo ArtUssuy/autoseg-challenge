bdc6b26bcddcb082e90f8f7bd6e73df899e93c2f
Arthur Nakano <arthurnakano@Leticias-MacBook-Air.local>
2019-09-21 14:31:33 -0300

    implementada funcao de criacao de novas tasks no DOM

--- js/createList.js
+++ js/createList.js
@@ -1,4 +1,6 @@
 (function () { 
+    
+    document.addEventListener("DOMContentLoaded", loadPage);
 
     const lists = [
         {
@@ -8,24 +10,56 @@
         }
     ]
 
-    document.addEventListener("DOMContentLoaded", loadPage);
+    const inputsTasksArray = []
 
+    // FUNCTION TRIGGERED WHEN DOM IS LOADED
     function loadPage() {
-        const mainForm = document.getElementById("mainForm");
-        mainForm.addEventListener("submit", submitForm)
-        addFunctionToAddTask()
+        mainFormSubmit()
+        
         createListContent()
+        createInputTaskDom()
     }
 
+    // CREATE FIRST INPUT INSIDE DOM
+    function createInputTaskDom() {
+        const firstInputDom = `<div class="add-task-wrapper">
+                                    <input value="" placeholder="Adicionar tarefa" type="text" class="namesTasks" id="namesTasks">
+                                    <button class="addTask" id="addTask" type="button">
+                                        <img id="addTask-icon" src="/media/assets/botao_adicionar.png" alt="">
+                                    </button>
+                                </div>`
+            
+
+        document.getElementById("add-task-container").innerHTML = firstInputDom
+        addFunctionToLastInput()
+    }
+
+    // ADD INPUT TASK DOM AND VALUES
+    function addTask() {
+        newInputDom = document.getElementById("add-task-container").lastChild.cloneNode(true)
+        newInputDom.firstElementChild.value = ""
+        document.getElementById("add-task-container").appendChild(newInputDom)
+        addFunctionToLastInput()
+    }
+
+    // ADD LISTENER TO MAIN FORM SUBMIT
+    function mainFormSubmit() {
+        const mainForm = document.getElementById("mainForm");
+        mainForm.addEventListener("submit", submitForm)
+    }
+
+    // RETURN VALUES FROM MAIN FORM
     function getValues() {
         const nameList = document.getElementById("nameList").value
         const namesTasks = document.getElementById("namesTasks").value
+        getValuesFromTasksInputs()
         return {
             nameList: nameList,
             namesTasks: namesTasks
         }
     }
 
+    // MAIN FORM SUBMIT FUNCTION
     function submitForm(e) {
         e.preventDefault()
         const newList = {
@@ -37,6 +71,7 @@
         createListContent()
     }
 
+    // CREATE CONTENT TO LIST OF LISTS
     function createListContent() {
         const newListDom = lists.map(a => {
             return `<div class="list-wrapper" id="list-wrapper">
@@ -76,26 +111,18 @@
         })
         document.getElementById("lists-wrapper").innerHTML = newListDom
     }
-    
-    function addTask() {
-        console.log("add Task function")
-        const inputTaskDom = 
-        `<div class="add-task-wrapper">
-            <input placeholder="Adicionar tarefa" type="text" class="namesTasks" id="namesTasks">
-            <button class="addTask" id="addTask" type="button">
-                <img id="addTask-icon" src="/media/assets/botao_adicionar.png" alt="">
-            </button>
-        </div>`
-        
-        document.getElementById("add-task-container").innerHTML += inputTaskDom
-        addFunctionToAddTask()
-    }
 
-    function addFunctionToAddTask() {
+    // ADD LISTENER ONLY TO THE LAST TASK INPUT
+    function addFunctionToLastInput() {
         const addTaskBtn = document.getElementsByClassName("addTask");
         addTaskBtn[addTaskBtn.length-1].addEventListener("click", addTask)
     }
 
+    // RETURN VALUES FROM THE TASKS INPUTS
+    function getValuesFromTasksInputs() {
+        const taskInputs = document.getElementsByClassName("namesTasks")
+        console.log(taskInputs)
+    }
 
 })();
 
