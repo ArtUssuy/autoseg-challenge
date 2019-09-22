4b2d1484b575aff1319b75d23e68ef4ea7e05579
Arthur Nakano <arthurnakano@Leticias-MacBook-Air.local>
2019-09-21 23:10:37 -0300

    implementada funcao para excluir tarefas

--- js/createList.js
+++ js/createList.js
@@ -1,20 +1,76 @@
 (function () { 
     
+/* FUNCTION AND VARIABLES ON LOAD *************************************************************** */ 
+
+    // TRIGGER FUNCTION ON DOM LOAD
     document.addEventListener("DOMContentLoaded", loadPage);
 
+    // VARIABLE THAT STORES LISTS AND TASKS
     const lists = [
         {
             listId: 0,
             nameList: "Primeira Lista",
-            namesTasks: ["Tarefa 01 ", "Tarefa 02"]
+            namesTasks: ["Tarefa 01", "Tarefa 02"]
         }
     ]
 
-    // FUNCTION TRIGGERED WHEN DOM IS LOADED
+    // FUNCTIONS TRIGGERED WHEN DOM IS LOADED
     function loadPage() {
         mainFormSubmit()
         createListContent()
         createInputTaskDom()
+        addListenerDeleteTask()
+    }
+
+    function addListenerDeleteTask() {
+        const deleteTaskButtons = document.querySelectorAll("button.delete-task")
+        deleteTaskButtons.forEach((deleteTaskButton) => {
+            deleteTaskButton.addEventListener("click", deleteTask);
+        });
+    }
+
+    // CREATE CONTENT TO LIST OF LISTS AND TASKS
+    function createListContent() {
+        const newListDom = lists.map(a => {
+            return `<div class="list-wrapper" id="list-wrapper">
+                        <div class="list">
+                            <div class="list-details-wrapper">
+                                <img src="" alt="">
+                                <span id="listName">${a.nameList}</span>
+                            </div>
+
+                            <div class="list-button-wrapper ">
+                                <button id="editList" class="edit-list" >
+                                    <img src="/media/assets/icone_editar.png" alt="Editar lista">
+                                </button>
+                                <button id="deleteList" class="delete-list"" >
+                                    <img src="/media/assets/icone_deletar_lista.png" alt="Excluir lista">
+                                </button>
+                            </div>
+                        </div>
+                        ${a.namesTasks.map((c) => {
+                            return `<div class="tasks">
+                                <div class="task-details-wrapper">
+                                    <input type="checkbox">
+                                    <span id="taskName">${c}</span>
+                                </div>
+
+                                <div class="task-button-wrapper">
+                                    <button id="deleteTask" class="delete-task"" >
+                                        <img src="/media/assets/icone_deletar_lista.png" alt="Excluir tarefa">
+                                    </button>
+                                </div>
+                            </div>`
+                        }).join('')}
+                    </div>`
+        }).join('')
+        document.getElementById("lists-wrapper").innerHTML = newListDom
+    }
+
+    // ADD LISTENER TO MAIN FORM SUBMIT
+    function mainFormSubmit() {
+        const mainForm = document.getElementById("mainForm");
+        mainForm.addEventListener("submit", submitForm)
     }
 
     // CREATE FIRST INPUT INSIDE DOM
@@ -31,19 +87,57 @@
         addFunctionToLastInput()
     }
 
-    // ADD INPUT TASK DOM AND VALUES
+
+/* INPUT TASKS FUNCTIONS ************************************************************************ */ 
+
+    // ADD INPUT TASK TO DOM
     function addTask() {
         newInputDom = document.getElementById("add-task-container").lastChild.cloneNode(true)
         newInputDom.firstElementChild.value = ""
         document.getElementById("add-task-container").appendChild(newInputDom)
         addFunctionToLastInput()
+        addListenerDeleteTask()
     }
 
-    // ADD LISTENER TO MAIN FORM SUBMIT
-    function mainFormSubmit() {
-        const mainForm = document.getElementById("mainForm");
-        mainForm.addEventListener("submit", submitForm)
+    function deleteTask(e) {
+
+        e.path.map((path) => {
+            if(path.className === "tasks") {
+                const spanTaskName = path.firstElementChild.lastElementChild.textContent
+                lists.map(listTaskName => {
+                    listTaskName.namesTasks.map((nameTask, b) => {
+                        if (nameTask === spanTaskName) {
+                            path.remove()
+                            listTaskName.namesTasks.splice(b, 1)
+                        }
+                    })
+                })
+            }
+        })
+        console.log(lists)
     }
+
+    // ADD LISTENER ALWAYS TO THE LAST TASK INPUT
+    function addFunctionToLastInput() {
+        const addTaskBtn = document.getElementsByClassName("addTask");
+        addTaskBtn[addTaskBtn.length-1].addEventListener("click", addTask)
+    }
+
+/* LIST ADD FUNCTIONS ************************************************************************ */ 
+
+    // MAIN FORM SUBMIT FUNCTION
+    function submitForm(e) {
+        e.preventDefault()
+        const newList = {
+            listId: lists.length + 1,
+            nameList: getValues().nameList,
+            namesTasks: getValues().namesTasks
+        }
+        lists.push(newList)
+        createListContent()
+        addListenerDeleteTask()
+    }
+
 
     // RETURN VALUES FROM MAIN FORM
     function getValues() {
@@ -60,64 +154,13 @@
         }
     }
 
-    // MAIN FORM SUBMIT FUNCTION
-    function submitForm(e) {
-        e.preventDefault()
-        const newList = {
-            listId: lists.length + 1,
-            nameList: getValues().nameList,
-            namesTasks: getValues().namesTasks
-        }
-        lists.push(newList)
-        createListContent()
-    }
-
-    // CREATE CONTENT TO LIST OF LISTS
-    function createListContent() {
-        const newListDom = lists.map(a => {
-            return `<div class="list-wrapper" id="list-wrapper">
-                        <div class="list">
-                            <div class="list-details-wrapper">
-                                <img src="" alt="">
-                                <span id="listName">${a.nameList}</span>
-                            </div>
-
-                            <div class="list-button-wrapper ">
-                                <button id="editList" class="edit-list" >
-                                    <img src="" alt="">
-                                </button>
-                                <button id="deleteList" class="delete-list"" >
-                                    <img src="" alt="">
-                                </button>
-                            </div>
-                        </div>
-                        ${a.namesTasks.map((c) => {
-                            return `<div class="tasks">
-                                <div class="task-details-wrapper">
-                                    <input type="checkbox">
-                                    <span id="taskName">${c}</span>
-                                </div>
-
-                                <div class="task-button-wrapper">
-                                    <button id="editList" class="edit-list" >
-                                        <img src="" alt="">
-                                    </button>
-                                    <button id="deleteList" class="delete-list"" >
-                                        <img src="" alt="">
-                                    </button>
-                                </div>
-                            </div>`
-                        }).join('')}
-                    </div>`
-            
-        })
-        document.getElementById("lists-wrapper").innerHTML = newListDom
-    }
-
-    // ADD LISTENER ONLY TO THE LAST TASK INPUT
-    function addFunctionToLastInput() {
-        const addTaskBtn = document.getElementsByClassName("addTask");
-        addTaskBtn[addTaskBtn.length-1].addEventListener("click", addTask)
-    }
-
 })();
+
+
+/*
+
+- IMPLEMENTAR FUNCOES DE EXCLUIR E EDITAR 
+
+- FUNCOES ESTAO DESORGANIZADAS
+    - ENTENDER TUDO QUE DIGITEI E ORGANIZAR
+*/
